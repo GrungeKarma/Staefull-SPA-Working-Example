c54fc8251058d9d615389edfcd8e5bc1921843d2
Anthony Lopez <grungekarma@gmail.com>
2021-05-25 20:45:35 -0500

    bio

---
+++ components/views/Blogs.js
@@ -0,0 +1,27 @@
+import html from "html-literal";
+
+export default st => html`
+  <section id="blog">
+    ${st.posts
+      .map(post => {
+        format;
+      })
+      .join()}
+  </section>
+`;
+function formatBlogPost(post) {
+  return `
+  <div class="blog-post">
+    <h4>${post.title} by User ${post.userId}</h4>
+    <p>${post.body}</p>
+  </div>`;
+}
+
+export default (st) => `
+<section id="blog">
+${st.posts
+  .map((post) => {
+    formatBlogPost(post);
+  })
+  .join()}
+</section>`;
